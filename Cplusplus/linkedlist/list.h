43deea298d3b275ec2abb5b74ac71b8f53a57b4a
Pritpal <pritpal20@gmail.com>
2022-12-21 11:56:11 +0530

    added linked list

--- Cplusplus/linkedlist/list.h
+++ Cplusplus/linkedlist/list.h
@@ -1,9 +1,42 @@
 #pragma once
+#include <iostream>
 
-class list
-{
+struct node {
+	int data;
+	node* next;
+
+	node(int _data) : data(_data),next(nullptr) {
+
+	} 
+};
+
+class list {
+
+private : 
+	node* root;
 public:
-	list();
-	~list();
+	list() : root(nullptr) {
+
+	}
+	~list() {
+		std::cout << "object deleted \n" ;
+	}
+
+	void insert(int data) {
+		node* temp = root;
+		node* new_node = new node(data);
+		root = new_node;
+		new_node->next = temp;
+	}
 	
+	void print() {
+
+		node* current = root;
+
+		while(current) {
+			std::cout << current->data << "->";
+			current = current->next;
+		}
+		std::cout << "null" << std::endl; 
+	}
 };
--- Cplusplus/linkedlist/main.cpp
+++ Cplusplus/linkedlist/main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	list l1;
+	l1.insert(10);
+	l1.insert(20);
+
+	l1.print();
 	return 0;
 }
